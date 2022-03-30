a1adbd80215845687258e8620434bb9b1706ddf4
kapilmundada <kapilmundada21@gmail.com>
2022-03-30 21:18:23 +0530

    .

--- backend/login_registration/welcome.js
+++ backend/login_registration/welcome.js
@@ -1,6 +1,6 @@
 firebase.auth().onAuthStateChanged((user)=>{
     if(!user){
-        location.replace("../login.php")
+        location.replace("../login.html")
     }else{
         document.getElementById("user").innerHTML = "Hello, " + user.email
         document.getElementById("name").innerHTML = user.email
